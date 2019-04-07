a7e4a4c78fb472e8c8cf8de27bf3f1cf41700678
PETREKILLAH <Japanboysushi@gmail.com>
2019-04-06 21:59:01 -0400

    Add Client code logic in game1

--- VRChat2/VRChat2/Game1.cs
+++ VRChat2/VRChat2/Game1.cs
@@ -5,6 +5,60 @@ using System.Net;
 using System.Net.Sockets;
 using System.Collections.Generic;
 
+
+struct MoveYou
+{
+    public int x;
+    public int y;
+    public int id;
+
+    public MoveYou(int x, int y, int id)
+    {
+        this.x = x;
+        this.y = y;
+        this.id = id;
+    }
+
+
+}
+
+struct MoveOther
+{
+    public int x;
+    public int y;
+    public int id;
+
+    public MoveOther(int x, int y, int id)
+    {
+        this.x = x;
+        this.y = y;
+        this.id = id;
+    }
+}
+
+struct SendingClientInfo
+{
+    
+    public int x;
+    public int y;
+    public int w;
+    public int h;
+    public int id;
+    public int spriteID;///CHANGE THIS TO ENUM AFTER WE MERGE
+    public Color color;
+
+    public SendingClientInfo(int x,int y, int w, int h, int id, int spriteID,Color color)
+    {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.id = id;
+        this.spriteID = spriteID;
+        this.color = color;
+    }
+}
+
 namespace VRChat2
 {
     public enum Command
@@ -12,14 +66,23 @@ namespace VRChat2
         MoveMe,//Client => Server (newX,newY)
         MoveYou,// Server => Client (newX,newY,id)
         MoveOther, // Server => Client (newX,newY,id)
-    }
+        SendingClientInfo,// Server => Client [(x1,y1,id,sprite,color),(x2,y2,id,sprite,color),(x3,y3,id,sprite,color)...]
 
+    }
     public enum Assets
     {
         circle,
         triangle,
         square,
     }
+    
+    public enum GameState
+    {
+        waiting,
+        playing,
+    }
+
+    
     /// <summary>
     /// This is the main type for your game.
     /// </summary>
@@ -27,6 +90,15 @@ namespace VRChat2
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+        GameState gameState;
+
+        static public bool SERVER;
+
+
+        /// <summary>
+        /// ClientCurrentCommand Being sent from the server
+        /// </summary>
+        static public string CurrentCommand = null;
 
         /// <summary>
         /// The relational database of textures
@@ -43,8 +115,14 @@ namespace VRChat2
         /// </summary>
         static public DrawnEnt ply;
 
+        /// <summary>
+        /// ClientSide Client object 
+        /// </summary>
+        Client MyClient;
+
         public Game1()
         {
+            gameState = GameState.waiting;
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             string ip = GetIpAddress();
@@ -53,14 +131,16 @@ namespace VRChat2
             if (ip == "129.21.50.210")
             {
                 ip = "129.21.50.210";
+                SERVER = true;
                 System.Console.WriteLine("Creating a server");
                 Server server = new Server(IPAddress.Parse(ip), 8888);
             }
             else
             {
                 ip = "129.21.50.210";
+                SERVER = false;
                 System.Console.WriteLine("Creating a client");
-                Client client = new Client(IPAddress.Parse(ip), 8888);
+                Client MyClient = new Client(IPAddress.Parse(ip), 8888);
             }
 
         }
@@ -113,8 +193,103 @@ namespace VRChat2
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == Microsoft.Xna.Framework.Input.ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Microsoft.Xna.Framework.Input.Keys.Escape))
                 Exit();
+
+
+
+            if (SERVER)
+            {
+                //IDK WHAT YOU DO HERE RIVER...
+            }
+            else
+            {
+                //PARSING INCOMMING DATA IF ANY
+                List<SendingClientInfo> clientInfo = null;
+                MoveOther moveOther;
+                MoveYou moveYou;
+
+                //Parse the data sent by the server
+                if (CurrentCommand != null)
+                {
+                    string[] tempCmdArgs = CurrentCommand.Split(',');
+                    int cmdHead = int.Parse(tempCmdArgs[0]);
+
+                    switch ((Command)cmdHead)
+                    {
+                        case Command.MoveOther:
+                            moveOther = new MoveOther(
+                                int.Parse(tempCmdArgs[1]),
+                                int.Parse(tempCmdArgs[2]),
+                                int.Parse(tempCmdArgs[3])
+                                );
+
+                            break;
+
+                        case Command.MoveYou:
+                            moveYou = new MoveYou(
+                                int.Parse(tempCmdArgs[1]), 
+                                int.Parse(tempCmdArgs[2]), 
+                                int.Parse(tempCmdArgs[3])
+                                );
+
+                            break;
+
+                        case Command.SendingClientInfo:
+                            clientInfo = new List<SendingClientInfo>();
+                            for (int i = 1; i < tempCmdArgs.Length; i++)
+                            {
+                                string[] ply = tempCmdArgs[i].Split(':');
+                                SendingClientInfo plyStruct = new SendingClientInfo(
+                                    int.Parse(ply[0]),
+                                    int.Parse(ply[1]),
+                                    int.Parse(ply[2]),
+                                    int.Parse(ply[3]),
+                                    int.Parse(ply[4]),
+                                    int.Parse(ply[5]),
+                                    new Color(int.Parse(ply[6]),int.Parse(ply[7]),int.Parse(ply[8]))
+                                    );
+                                clientInfo.Add(plyStruct);
+                            }
+                            break;
+
+                        case Command.MoveMe:
+                            System.Console.WriteLine("Why did the server send me this?");
+                            break;
+                    }
+                    
+                }
+                
+                switch (gameState)
+                {
+                    case GameState.waiting:
+                        System.Console.WriteLine("Got data");
+                        //If we got client info from the server, lets populate drawnEnts array
+                        if (clientInfo != null)
+                        {
+                            gameState = GameState.playing;
+                            foreach(SendingClientInfo s in clientInfo)
+                            {
+                                Ents.Add(
+                                    new DrawnEnt(
+                                        new Rectangle(s.x, s.y, s.w, s.h),
+                                        assestsDict[(Assets)s.spriteID],
+                                        s.color,
+                                        s.id
+                                        ));
+                                        
+                            }
+                        }
+
+                        break;
+                    case GameState.playing:
+                        
+                        //MyClient.Send();
+                        break;
+                }
+                //Clear the current command.
+                CurrentCommand = null;
+            }
 
             //client.
 
